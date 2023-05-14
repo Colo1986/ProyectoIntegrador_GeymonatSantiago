b3425879329acf9b28fa257fe94fb3fe151b677e
Colo1986 <118395055+Colo1986@users.noreply.github.com>
2023-05-14 12:01:57 -0300

    Back

---
+++ ProyectoIntegrador_GeymonatSantiago/BackEnd/src/main/java/com/portfolio/sjg/SjgApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.sjg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SjgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SjgApplication.class, args);
+	}
+
+}
