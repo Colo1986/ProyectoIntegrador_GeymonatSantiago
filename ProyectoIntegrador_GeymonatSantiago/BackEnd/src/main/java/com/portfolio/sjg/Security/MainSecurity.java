35f067c72c885b43aef14fa832f0bfbbfbc13079
Colo1986 <118395055+Colo1986@users.noreply.github.com>
2023-06-29 09:05:21 -0300

    Update MainSecurity.java

--- ProyectoIntegrador_GeymonatSantiago/BackEnd/src/main/java/com/portfolio/sjg/Security/MainSecurity.java
+++ ProyectoIntegrador_GeymonatSantiago/BackEnd/src/main/java/com/portfolio/sjg/Security/MainSecurity.java
@@ -7,37 +7,26 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
-@EnableMethodSecurity(prePostEnabled = true)
+@EnableMethodSecurity
 public class MainSecurity {
-
     @Autowired
     UserDetailsImpl userDetailsServiceImpl;
+
     @Autowired
     JwtEntryPoint jwtEntryPoint;
 
-    @Bean
-    public SecurityFilterChain filterChain (HttpSecurity http) 
-           throws Exception{
-        return http.build();
-    }
-    
-    @Bean
-    public WebSecurityCustomizer webSecurityCustomizer () {
-        return (web) -> web.ignoring().requestMatchers("/images/**");
-    }
-        
     @Bean
     public JwtTokenFilter jwtTokenFilter() {
         return new JwtTokenFilter();
@@ -47,20 +36,24 @@ public class MainSecurity {
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
-    
-    @Override
-    protected AuthenticationManager authenticationManager() throws Exception {
-        return super.authenticationManagerBean();
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
+            throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
     }
-    
-    @Override
-    public AuthenticationManager authenticationManagerBean() throws Exception {
-        return super.authenticationManagerBean();
-    }
-    
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth)throws Exception {
-        auth.userDetailsService(userDetailsServiceImpl).passwordEncoder(passwordEncoder());
-        
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.cors().and().csrf().disable()
+                .exceptionHandling().authenticationEntryPoint(jwtEntryPoint).and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+                .authorizeHttpRequests()
+                .requestMatchers("/**").permitAll()
+                .anyRequest().authenticated();
+
+        http.addFilterBefore(jwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
     }
 }
